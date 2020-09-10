bd5d480fe3c2e7b4ca7a4f95ede3ee483c86d571
3ndrius <andrzejo1991@gmail.com>
2020-09-10 22:22:41 +0200

    Adding next test click event normal and with delay (wait for el val)

--- src/components/Click/__tests__/index.test.js
+++ src/components/Click/__tests__/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent, waitForElement } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect'
 import Click from '../index'
 
@@ -15,5 +15,20 @@ describe('render click component', () => {
     it('display count value ', () => {
         const { getByTestId } = render(<Click />);
         expect(getByTestId('counter-value')).toHaveTextContent('0');
+    })
+
+    it('increment count value', () => {
+        const { getByTestId, getByText } = render(<Click />);
+        fireEvent.click(getByText("Up"));
+        expect(getByTestId('counter-value')).toHaveTextContent("1");
+    })
+
+    it('decrement count value', async () => {
+        const { getByTestId, getByText } = render(<Click />);
+        fireEvent.click(getByText("Down"));
+
+        const countDelay = await waitForElement(() => getByText("-1"));
+        expect(countDelay).toHaveTextContent("-1");
+        
     })
 })
--- src/components/Click/index.js
+++ src/components/Click/index.js
@@ -2,10 +2,18 @@ import React from 'react'
 
 export default function Click() {
     const [ valueClick, setValueClick ] = React.useState(0)
+    const increment = () => {
+        setValueClick(valueClick + 1 )
+    }
+    const decrement = () => {
+        setTimeout(()=>{
+            setValueClick(valueClick -1 )
+        },300)
+    }
     return (
         <div className="click">
-            <button>Up</button>
-            <button>Down</button>
+            <button onClick={increment}>Up</button>
+            <button onClick={decrement}>Down</button>
             <h2 data-testid="counter-value">{valueClick}</h2>
         </div>
     )
